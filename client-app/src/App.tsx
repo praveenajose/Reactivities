f828c4cb8c965129d4f64d9c206821973b98cf64
Praveena Prosper <p.prosper@metricgroup.co.uk>
2025-01-02 15:02:47 +0000

    Temporary code for TSX learning

---
+++ client-app/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { ducks } from "./demo";
+import DuckItem from "./DuckItem";
+
+function App() {
+  return (
+    <div>
+      <h1>Reactivities</h1>
+      {ducks.map((duck) => (
+        <DuckItem key={duck.name} duck={duck} />
+      ))}
+    </div>
+  );
+}
+
+export default App;
